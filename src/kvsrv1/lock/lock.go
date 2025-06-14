93d23ed4da97072a7fb89db38b7f6bf74dc471e9
yellow <yellow@192.168.1.7>
2025-06-14 13:34:57 +0800

    lock

--- src/kvsrv1/lock/lock.go
+++ src/kvsrv1/lock/lock.go
@@ -1,6 +1,7 @@
 package lock
 
 import (
+	"6.5840/kvsrv1/rpc"
 	"6.5840/kvtest1"
 )
 
@@ -9,7 +10,9 @@ type Lock struct {
 	// the specific Clerk type of ck but promises that ck supports
 	// Put and Get.  The tester passes the clerk in when calling
 	// MakeLock().
-	ck kvtest.IKVClerk
+	ck  kvtest.IKVClerk
+	id  string
+	key string
 	// You may add code here
 }
 
@@ -21,13 +24,39 @@ type Lock struct {
 func MakeLock(ck kvtest.IKVClerk, l string) *Lock {
 	lk := &Lock{ck: ck}
 	// You may add code here
+	lk.id = kvtest.RandValue(8)
+	lk.key = l
 	return lk
 }
 
 func (lk *Lock) Acquire() {
+	//func (ck *Clerk) Get(key string) (string, rpc.Tversion, rpc.Err)
+	//func (ck *Clerk) Put(key, value string, version rpc.Tversion) rpc.Err {
 	// Your code here
+	for {
+		value, version, err := lk.ck.Get(lk.key)
+		if err == rpc.ErrNoKey {
+			err := lk.ck.Put(lk.key, lk.id, 0)
+			if err == rpc.OK {
+				return
+			}
+			//	如果加锁成功 就返回，否则继续自转重试
+		} else if value == "" {
+			err := lk.ck.Put(lk.key, lk.id, version)
+			if err == rpc.OK {
+				return
+			}
+		}
+	}
 }
 
 func (lk *Lock) Release() {
+	//释放锁
 	// Your code here
+	value, version, err := lk.ck.Get(lk.key)
+	if err == rpc.ErrNoKey || lk.id != value {
+		return
+	}
+
+	lk.ck.Put(lk.key, "", version)
 }
